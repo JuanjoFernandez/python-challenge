64eb09e6bfd28869ffcb2903dc6e93a170b0d1e1
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-03-24 11:47:26 -0600

    input added to pyparagraph

---
+++ PyParagraph/main.py
@@ -0,0 +1,10 @@
+#importing libraries
+import os
+
+#asking the user for the path and name of the file to be analyzed
+print("============================================")
+print("|                                          |")
+print("|  Welcome to the paragraph analysis tool  |")
+print("|                                          |")
+print("============================================")
+file = input("What's the name of the file you want to analyze? (in Resources folder):")
