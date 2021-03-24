dc5f8db46bab78056a0e843099b51daa1e5b5c0e
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-03-24 11:58:14 -0600

    added error handler for non-existing file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -8,3 +8,14 @@ print("|  Welcome to the paragraph analysis tool  |")
 print("|                                          |")
 print("============================================")
 file = input("What's the name of the file you want to analyze? (in Resources folder):")
+
+txtpath = os.path.join('Resources', file)
+#checking if the file exists
+file_exists = False
+while file_exists == False:
+    file_exists = os.path.exists(txtpath)
+    if file_exists == False:
+        file = input("Error: file doesn't exist, make sure it's on the /Resources folder and is typed correctly:")
+        txtpath = os.path.join('Resources', file)
+
+
